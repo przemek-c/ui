f3351340881ca5723acefa41cefd2e16b40b9e9b
przemek-c <105457222+przemek-c@users.noreply.github.com>
2024-10-08 12:58:22 +0000

    p 351

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
+
+const Sidebar = () => {
+    const { width } = useWindowDimensions();
+    if (width <= 768) {
+        return null;
+    }
+    return <div className="sidebar">Sidebar</div>;
+};
+export default Sidebar;
